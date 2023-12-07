0e2007ffb02fa49ab59915a54b8f151a575657a3
JsTitan <sherazahmdd@gmail.com>
2023-12-07 13:26:23 +0500

    Session 4.

--- session-4/main.js
+++ session-4/main.js
@@ -1,18 +1,22 @@
-let watcher = false;
-function computeRadius() {
-    watcher = true;
-    return 4;
-}
+const first = document.querySelector('#number1');
+const second = document.querySelector('#number2');
 
-function drawCircle(radius) {
-    watcher = false;
-    return radius*360;
-}
+const result = document.querySelector('.result');
 
-function main () {
-    const radius = computeRadius();
+if (window.Worker) {
+  const myWorker = new Worker("worker.js");
 
-    while (watcher) {
-        drawCircle (radius);
-        }
+  [first, second].forEach(input => {
+    input.onchange = function() {
+      myWorker.postMessage([first.value, second.value]);
+      console.log('Message posted to worker');
+    }
+  })
+
+  myWorker.onmessage = function(e) {
+    result.textContent = e.data;
+    console.log('Message received from worker');
+  }
+} else {
+  console.log('Your browser doesn\'t support web workers.');
 }
